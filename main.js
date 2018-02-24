ea5605c7367dcf324afd8fdd13ac2b84d5661cf6
Xaconi <xaco.89@gmail.com>
2018-02-24 18:58:03 +0100

    Devuelve el mismo texto, y misma imagen a 256x256

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const apiKey = '';
 const TeleBot = require('telebot');
 const bot = new TeleBot(apiKey);
 
+const fs = require('fs');
+const Jimp = require("jimp");
+const request = require('request');
+
 bot.on(
 	'text', 
 	(msg) => msg.reply.text(msg.text)
@@ -10,10 +14,43 @@ bot.on(
 bot.on(
 	'photo', 
 	(msg) => {
-		// msg.reply.photo('https://api.telegram.org/file/bot' + apiKey + '/' + msg.photo[0].file_path);
-		console.log(msg.photo);
-		msg.reply.photo('http://thecatapi.com/api/images/get');
+		// Aconseguim la foto en bona qualitat
+		getRealPhoto(msg.photo[3], function(photo) {
+			photoEdition(photo);
+			msg.reply.photo('test.jpg');
+			// fs.unlink('test.jpg');
+		});
 	}
 );
 
 bot.start();
+
+// Funcions
+
+function photoEdition(photo){
+	Jimp.read('https://api.telegram.org/file/bot' + apiKey + '/' + photo.file_path, function(err, image) {
+		if(err) throw err;
+		image.resize(256, 256)
+			.write("test.jpg");
+	});
+}
+
+function getRealPhoto(photo, callback){
+	var url = 'https://api.telegram.org/bot' + apiKey + '/getFile?file_id=' + photo.file_id;
+	var realPhoto = '';
+	request.get({
+		url: url,
+		json: true,
+		headers: {'User-Agent': 'request'}
+	}, (err, res, data) => {
+		if (err) {
+	    	console.log('Error:', err);
+	    } else if (res.statusCode !== 200) {
+	    	console.log('Status:', res.statusCode);
+	    } else {
+	    	// data is already parsed as JSON:
+	    	realPhoto = data.result;
+	    	callback(realPhoto);
+	    }
+	});
+}
